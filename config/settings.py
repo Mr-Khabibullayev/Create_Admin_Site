56aeca2a064b1b218cd51622519e73da5f7ce168
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2022-12-16 20:21:20 +0500

    localhost checked

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ya4^hm8%)-$^+j8f5)va%f$5%6trln^0i262!!h(42s83)=1+2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
