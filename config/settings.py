1193873add49a94b985a419d6b49d28b8f31d0d2
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2022-12-16 20:25:26 +0500

    allowed-host cleaner

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news',
-]
-
+    'news',  
+]   
+    
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
